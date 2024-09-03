8ce6d35e937a33ebd44d99a8dc2f0fc4cfd67b2a
Juliocesar-silva <realcapetinha@gmail.com>
2024-09-03 19:01:35 -0300

    Média Aluno

--- Tipos-variaveis/src/App.java
+++ Tipos-variaveis/src/App.java
@@ -1,6 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println("Nova linha");
+        double primeiranota = 10;
+        double segundanota = 4;
+        double terceiranota = 7;
+        double media = Media(primeiranota, segundanota, terceiranota);
+        if(media < 7)
+        System.out.println("Reprovado com média: "+media);
+        else
+        System.out.println("Aprovado com média: "+media);
+    }
+    public static double Media(double nota1,double nota2,double nota3){
+        return (nota1 + nota2 + nota3)/3;
     }
 }
